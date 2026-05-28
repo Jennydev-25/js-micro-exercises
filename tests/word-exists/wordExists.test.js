e1a115ba54b66759b887a109598a5f42a9ec5d02
Jennydev-25 <jenny25sr@gmail.com>
2026-05-28 20:25:40 +0200

    test: add test for string not containing English returns false

--- tests/word-exists/wordExists.test.js
+++ tests/word-exists/wordExists.test.js
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest';
 import { wordExists } from '../../src/word-exists/wordExists.js';
 
 describe('wordExists', () => {
-    
+
   // Caso positivo: la cadena contiene "English"
   it('should return true when string contains English', () => {
     const str = 'abcEnglishdef';
@@ -11,4 +11,11 @@ describe('wordExists', () => {
     expect(result).toBe(expected);
   });
 
+  // Caso negativo: la cadena no contiene "English"
+  it('should return false when string does not contain English', () => {
+    const str = 'abcnEglishsef';
+    const expected = false;
+    const result = wordExists(str);
+    expect(result).toBe(expected);
+  });
 });
