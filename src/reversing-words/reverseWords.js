67d4b3c5ed42bc2911f4c501bfc5031a4b8b60ea
Jennydev-25 <jenny25sr@gmail.com>
2026-05-29 19:04:34 +0200

    refactor: simplify reverseWords with chained methods

--- src/reversing-words/reverseWords.js
+++ src/reversing-words/reverseWords.js
@@ -1,6 +1,3 @@
 export function reverseWords(str) {
-  const words = str.split(' ');
-  const reversed = words.reverse();
-  const result = reversed.join(' ');
-  return result;
+  return str.split(' ').reverse().join(' ');
 }
