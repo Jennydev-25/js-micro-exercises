be457037785ea7e1b8205e19d32bde90107ba50d
Jennydev-25 <jenny25sr@gmail.com>
2026-05-29 21:07:02 +0200

    refactor: extract noSheep condition to readable variable

--- src/counting-sheep/countAnimals.js
+++ src/counting-sheep/countAnimals.js
@@ -1,9 +1,7 @@
 export function countAnimals(animals) {
   const sheep = animals.filter(Boolean).length;
-
-  if (sheep === 0) {
-    return 'UPS!!! A pack of hungry wolves';
-  }
-
-  return `There are ${sheep} sheep in total`;
+  const noSheep = sheep === 0;
+  return noSheep
+    ? 'UPS!!! A pack of hungry wolves'
+    : `There are ${sheep} sheep in total`;
 }
