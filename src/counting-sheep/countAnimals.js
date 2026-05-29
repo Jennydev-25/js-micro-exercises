5b8dbdff1aba4b2df837534f690a246f0507d425
Jennydev-25 <jenny25sr@gmail.com>
2026-05-29 21:16:19 +0200

    feat: add case for more sheep than wolves

--- src/counting-sheep/countAnimals.js
+++ src/counting-sheep/countAnimals.js
@@ -1,7 +1,21 @@
 export function countAnimals(animals) {
   const sheep = animals.filter(Boolean).length;
+  const wolves = animals.filter(animal => animal === false).length;
   const noSheep = sheep === 0;
-  return noSheep
-    ? 'UPS!!! A pack of hungry wolves'
-    : `There are ${sheep} sheep in total`;
+  const noWolves = wolves === 0;
+  const sheepWin = sheep > wolves;
+
+  if (noSheep) {
+    return 'UPS!!! A pack of hungry wolves';
+  }
+
+  if (noWolves) {
+    return `There are ${sheep} sheep in total`;
+  }
+
+  if (sheepWin) {
+    return `${sheep} sheep escaped!!!`;
+  }
+
+  return `There are ${sheep} sheep in total`;
 }
