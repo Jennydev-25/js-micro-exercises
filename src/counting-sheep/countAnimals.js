b096b13ef021e67e8fd4eca212f67ff68e5e3b2e
Jennydev-25 <jenny25sr@gmail.com>
2026-05-29 21:01:41 +0200

    feat: add wolves case when no sheep found

--- src/counting-sheep/countAnimals.js
+++ src/counting-sheep/countAnimals.js
@@ -1,4 +1,9 @@
 export function countAnimals(animals) {
   const sheep = animals.filter(Boolean).length;
+
+  if (sheep === 0) {
+    return 'UPS!!! A pack of hungry wolves';
+  }
+
   return `There are ${sheep} sheep in total`;
 }
